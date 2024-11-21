dc4b3f4f5278f12b721886b855a2182119b853ca
ReneeGA2020 <karazhan@126.com>
2024-11-22 05:43:05 +0800

    Simplify DataTemplates and update template selection
    Removed ListViewItem, GridViewItem, and TreeViewItem wrappers around TextBlock elements in MainWindow.xaml to simplify DataTemplates. In TestTemplateSelector.cs, removed SelectTemplateCore method with single object parameter. Updated SelectTemplateCore method with object and DependencyObject parameters to call revised GetTemplate method. GetTemplate now also takes a DependencyObject parameter and sets ItemsSource for TreeViewItem containers when item is of type TestVM1 or TestVM2.

--- WinUIItemTemplateSelectorBug/TemplateSelector/TestTemplateSelector.cs
+++ WinUIItemTemplateSelectorBug/TemplateSelector/TestTemplateSelector.cs
@@ -8,23 +8,31 @@ public class TestTemplateSelector : DataTemplateSelector
     public DataTemplate? TemplateTestVM1 { get; set; }
 
     public DataTemplate? TemplateTestVM2 { get; set; }
-    protected override DataTemplate SelectTemplateCore(object item)
-    {
-        return GetTemplate(item) ?? base.SelectTemplateCore(item);
-    }
 
     protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
     {
-        return GetTemplate(item) ?? base.SelectTemplateCore(item, container);
+        return GetTemplate(item, container) ?? base.SelectTemplateCore(item, container);
     }
 
-    private DataTemplate? GetTemplate(object item)
+    private DataTemplate? GetTemplate(object item, DependencyObject container)
     {
-        return item switch
+        var template = item switch
         {
             TestVM1 => TemplateTestVM1,
             TestVM2 => TemplateTestVM2,
             _ => null,
         };
+        if(container is TreeViewItem element)
+        {
+            if (item is TestVM1 a)
+            {
+                element.ItemsSource = a.Items;
+            }
+            else if (item is TestVM2 b)
+            {
+                element.ItemsSource = b.Items;
+            }
+        }
+        return template;
     }
 }
